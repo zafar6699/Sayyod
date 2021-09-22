32bacb9d0fdab22d7dcb85beae5ec3fc0829fea2
Zafar6699 <webzafar6699@gmail.com>
2021-09-22 17:24:35 +0500

    asd

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Navbar />
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+
+    mounted() {},
+
+    methods: {}
+};
+</script>
+
+<style></style>
